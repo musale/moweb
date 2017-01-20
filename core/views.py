b2b79babf1b210e57679ca8d7d6e6f7b5972a54c
musale <martinmshale@gmail.com>
2017-01-20 18:53:02 +0300

    added the item view

--- core/views.py
+++ core/views.py
@@ -6,3 +6,9 @@ class LandingPage(TemplateView):
     """The landing page."""
 
     template_name = "core/index.html"
+
+
+class ItemView(TemplateView):
+    """The item page."""
+
+    template_name = "core/item.html"
