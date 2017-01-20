ad9fcb3d38168b24a3ac3867209094cd479ad9ba
musale <martinmshale@gmail.com>
2017-01-20 18:52:40 +0300

    added item url

--- core/urls.py
+++ core/urls.py
@@ -2,8 +2,9 @@
 
 from django.conf.urls import url
 
-from core.views import LandingPage
+from core.views import ItemView, LandingPage
 
 urlpatterns = [
-    url(r'$', LandingPage.as_view(), name='LandingPage'),
+    url(r'^$', LandingPage.as_view(), name='LandingPage'),
+    url(r'^item/(?P<id>[0-9]+)/$', ItemView.as_view(), name='item'),
 ]
