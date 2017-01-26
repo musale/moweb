89b30fe51e159f8ddf656fdb9f223b661a0265e9
musale <martinmshale@gmail.com>
2017-01-26 09:40:23 +0300

    :paw_prints:Added super admin logic
    easily add items and images from the django admin

--- core/admin.py
+++ core/admin.py
@@ -1,3 +1,7 @@
+"""Super admin site config."""
 from django.contrib import admin
 
-# Register your models here.
+from core.models import Item, ItemImage
+
+admin.site.register(ItemImage)
+admin.site.register(Item)
