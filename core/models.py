046ea613f04d51b327feeb297621ee8f20ff5670
musale <martinmshale@gmail.com>
2017-01-26 09:42:47 +0300

    :floppy_disk:Set up models for the Item and ItemImage
    tables for the Item added and the Images for the items

--- core/models.py
+++ core/models.py
@@ -1,5 +1,29 @@
+"""Models of the Items and Images for storing in the database."""
 from __future__ import unicode_literals
 
 from django.db import models
 
-# Create your models here.
+
+class Item(models.Model):
+    """Table for an item."""
+
+    name = models.CharField(max_length=250, )
+    description = models.TextField()
+    link = models.CharField(max_length=255)
+    price = models.CharField(max_length=255)
+    contact = models.TextField()
+
+    def __str__(self):
+        """Return value for the obj."""
+        return self.name
+
+
+class ItemImage(models.Model):
+    """An item's image."""
+
+    item = models.ForeignKey(Item,)
+    image = models.FileField("ItemImages", upload_to="items/%Y/%m/%d")
+
+    def __str__(self):
+        """Return value for the obj."""
+        return self.item
